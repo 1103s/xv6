1caa182d3d06bd113207a01bc01130ecf5d1bc8e
AlmondNMT <alexander.schwierz@student.nmt.edu>
2022-09-13 19:46:43 -0600

    added test for out of range priority in lab2test.c

--- lab2test.c
+++ lab2test.c
@@ -6,15 +6,29 @@ int stdout = 1;
 
 int main(int argc, char **argv)
 {
-    
+    int priority;
     //get priority of porcess pid 2, the shell, expects 20
-    printf(stdout, "priority of process 2 is %d \n", get_priority(2));
+
+    priority = get_priority(2);
+    printf(stdout, "priority of process 2 is %d \n", priority);
+
+    //check if priority out of range
+    if(priority > 19 || priority < -20)
+        printf("Priority is not a value between -20 and +19\n");
 
     //set priority of process with pid 2 to 12
     printf(stdout, "Setting process pid 2 to priority 12, set_priority returns %d\n", set_priority(2, 12));
 
     //get priority of this process again to confirm change
-    printf(stdout, "priority of process 2 AFTER setting is %d\n", get_priority(2));
+    priority = get_priority(2);
+    printf(stdout, "priority of process 2 AFTER setting is %d\n", priority);
+
+    //check if priority out of range
+    if(priority > 19 || priority < -20)
+        printf("Priority is not a value between -20 and +19\n");
+
+    
+
 
     exit();
 }
